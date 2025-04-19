a472171865e4ce764311506d294bd687b1eb04be
Sawsan Elsebaey <sawsanelsebay2@gmail.com>
2025-04-19 14:55:36 +0200

    Initial commit with mvc project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TechExpress.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
